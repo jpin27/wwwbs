45938e5b9333dc3b014c478c995afb860e568292
Hibat ullah <hibatullah@Hibats-MBP.gateway.pace.com>
2017-09-21 22:53:42 -0600

    Added divs in Styles.css and linked it to index.php

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <html>
  <head>
-  <title>First website yoooo</title>
+  <title>Wacky Waving Wellness Booking System</title>
+  <link rel="stylesheet.css" type="text/css" href="styles/styles.css"
  </head>
  <body>
+     <div id="container">
+     <div id="header">
+         <hi>Wacky Waving Wellness Booking System</hi>
+     </div>
  <?php echo '<p>Boop</p>'; ?> 
+     </div>
  </body>
 </html>
