077caaae4b52450dcf02c9f8b39a4bd6dc55da59
Hibat ullah <hibatullah@uofs-10-227-153-247.usask.ca>
2017-09-28 16:13:19 -0600

    Drop down list for doctor and date selection

--- index.php
+++ index.php
@@ -8,29 +8,28 @@
   <link rel="stylesheet" type="text/css" href="styles/styles.css" />
  </head>
  <body>
-     <div id="container">
+    <div id="container">
      <div id="header">
          <h1>Wellness Centre</h1>
      </div>
-     <form>
-    <div id="content">
-     	 <select name="Choose Doctor">
-             <option>Choose Doctor</option>
-             <option>Dr Tayyab</option>
-             <option>Dr Thrwat</option>
-             <option>Dr Hiba</option>
-             <option>Dr Jay</option>
-             <option>Dr Fahd</option>
-             <option>Dr Brennan</option>
-             <option>Dr Charles</option>
-             <option>Dr Jesse</option>
-         </select>
-         
-         
-         </div>
-         </form>
-        
-
+     
+        <div class="docSelector">
+            <select>
+                <option> Choose Doctor </option>
+                <option>World</option>
+                <option>Welcome</option>
+                <option>Here</option>
+            </select>
+            </div>
+            <div class="dateSelector">
+             <select>
+                <option> Date </option>
+                <option>World</option>
+                <option>Welcome the dsdhadjhdadhh</option>
+                <option>Here</option>
+            </select>
+            </div>
+</div>
      </div>
  </body>
 </html>
