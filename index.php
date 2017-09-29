08554c82b5de5ee4ab43dee0a41b91674f0d496d
Hibat ullah <hibatullah@uofs-10-228-2-3.usask.ca>
2017-09-29 15:47:32 -0600

    Inserted basic Table for appointment timings

--- index.php
+++ index.php
@@ -29,6 +29,90 @@
                 <option>Here</option>
             </select>
             </div>
+            <table style="margin: 20px 0px" border="1px solid">
+            	<thead>
+            		<tr>
+            			<th>Time</th>
+            			<th style="width: 100%; text-align: left">Dr. Tayyab
+            			</th>
+            		</tr>
+            		<tbody>
+            			<tr>
+            				<td>9:00am</td>
+            				<td>Closed</td>
+            			</tr>
+            			<tr>
+            				<td>9:30am</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>10:00am</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>10:30am</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>11:00am</td>
+            				<td>Booked</td>
+            			</tr>
+            			<tr>
+            				<td>11:30am</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>12:00pm</td>
+            				<td>Lunch Hour</td>
+						</tr>
+            			<tr>
+            				<td>1:00pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>1:30pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>2:00pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>2:30pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>3:00pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>3:30pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>4:00pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>4:30pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>5:00pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>5:30pm</td>
+            				<td>Available</td>
+            			</tr>
+            			<tr>
+            				<td>6:00pm</td>
+            				<td>Closed</td>
+            			</tr>
+            			
+            		</tbody>
+            	</thead>
+            </table>
 </div>
      </div>
  </body>
