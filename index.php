c523731a183e4700be76637b8fc73f6803298945
Hibat ullah <hibatullah@Hibats-MBP.gateway.pace.com>
2017-10-02 22:06:41 -0600

    Basic layout for the Booking page

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
  <body>
     <div id="container">
      <div id="header">
-         <h1>Wellness Centre</h1>
+         <h1>Wacky Waving Wellness Centre</h1>
      </div>
      
         <div class="docSelector">
@@ -29,91 +29,113 @@
                 <option>Here</option>
             </select>
             </div>
-            <table style="margin: 20px 0px" border="1px solid">
+      <table>
             	<thead>
             		<tr>
             			<th>Time</th>
-            			<th style="width: 100%; text-align: left">Dr. Tayyab
+            			<th style="width: 95%; text-align: left">Dr. Tayyab
             			</th>
             		</tr>
             		<tbody>
             			<tr>
-            				<td>9:00am</td>
+            				<td>09:00AM</td>
             				<td>Closed</td>
             			</tr>
             			<tr>
-            				<td>9:30am</td>
+            				<td>09:30AM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>10:00am</td>
+            				<td>10:00AM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>10:30am</td>
+            				<td>10:30AM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>11:00am</td>
+            				<td>11:00AM</td>
             				<td>Booked</td>
             			</tr>
             			<tr>
-            				<td>11:30am</td>
+            				<td>11:30AM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>12:00pm</td>
-            				<td>Lunch Hour</td>
+            				<td>12:00PM</td>
+            				<td rowspan="2" style="vertical-align: baseline">Lunch Hour</td>
+            				
 						</tr>
+           			<tr>
+            					<td>12:30PM</td>
+            				</tr>
             			<tr>
-            				<td>1:00pm</td>
+            				<td>1:00PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>1:30pm</td>
+            				<td>1:30PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>2:00pm</td>
+            				<td>2:00PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>2:30pm</td>
+            				<td>2:30PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>3:00pm</td>
+            				<td>3:00PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>3:30pm</td>
+            				<td>3:30PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>4:00pm</td>
+            				<td>4:00PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>4:30pm</td>
+            				<td>4:30PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>5:00pm</td>
+            				<td>5:00PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>5:30pm</td>
+            				<td>5:30PM</td>
             				<td>Available</td>
             			</tr>
             			<tr>
-            				<td>6:00pm</td>
+            				<td>6:00PM</td>
             				<td>Closed</td>
             			</tr>
             			
             		</tbody>
             	</thead>
             </table>
+            <nav>
+            <ul>
+           <li> <a href="#">Log In</a></li>
+           <li><a href="#">Search Practitioners</a></li>
+           <li> <a href="#">My Bookings</a></li>
+            </ul>
+            </nav>
 </div>
+	<div id="footer">
+		<ul>
+		<a href="#">Contact</a>
+		<a href="#">Feedback</a>
+		</ul>
+		<ul>
+		<a href="#">Disclaimer</a>
+		<a href="#">Privacy</a>
+		&copy;Wacky Waving Wellnes Booking Center
+		</ul>
+	</div>
      </div>
  </body>
 </html>
