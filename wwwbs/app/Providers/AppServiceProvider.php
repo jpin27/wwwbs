0b9574b9f95a22e306a587dd47d98b303f8d4af0
Jude Pineda <jay.p@usask.ca>
2017-11-07 21:00:17 -0600

    Updated splash page and looked at web routes

--- wwwbs/app/Providers/AppServiceProvider.php
+++ wwwbs/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // @Jay 
+        // some workaround to fix the PHP migration error.
+        // Magic. Don't touch.
+
+        Schema::defaultStringLength(191);
     }
 
     /**
