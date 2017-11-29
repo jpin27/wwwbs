ff863d498cee22ac0cc49b683b74e7cccc40cfc2
Hibat ullah <hih050@mail.usask.ca>
2017-11-29 14:09:53 -0600

    Minor changes

--- wwwbs/app/User.php
+++ wwwbs/app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'firstName', 'lastName', 'email', 'password', 'subRole', 
+        'firstName', 'lastName', 'email', 'password', 'subRole',
         'nsid', 'healthNumber', 'birthDate', 'address',
     ];
 
@@ -41,10 +41,10 @@ class User extends Authenticatable
     public function authorizeRoles($roles)
     {
       if (is_array($roles)) {
-          return $this->hasAnyRole($roles) || 
+          return $this->hasAnyRole($roles) ||
                  abort(401, 'This action is unauthorized.');
       }
-      return $this->hasRole($roles) || 
+      return $this->hasRole($roles) ||
              abort(401, 'This action is unauthorized.');
     }
 
--- wwwbs/resources/views/layouts/navbar.blade.php
+++ wwwbs/resources/views/layouts/navbar.blade.php
@@ -34,7 +34,7 @@
                             <a class="nav-link js-scroll-trigger" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                          document.getElementById('logout-form').submit();">
-                                LOGOUT
+                                {{Auth::user()->firstName}}
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
