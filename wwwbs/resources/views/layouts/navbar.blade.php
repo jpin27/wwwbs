1bb216d8d15530b1e0c0ffb0795c593898911259
Tayyab Ahmed (taa632) <t.a@usask.ca>
2017-11-29 13:50:20 -0600

    matched the navbar to jay's navbar

--- wwwbs/resources/views/layouts/navbar.blade.php
+++ wwwbs/resources/views/layouts/navbar.blade.php
@@ -1,7 +1,13 @@
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-shrink" id="mainNav">
   <div class="container">
-    <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="/img/uofslogo-black.png" height="40"></a>
+    <a class="navbar-brand js-scroll-trigger" href="{{ url('/') }}">
+
+
+
+      <img src="/img/uofslogo-black.png" height="40">
+
+    </a>
 
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -23,29 +29,41 @@
         </li>
 
         <ul class="nav navbar-nav navbar-right">
+
             <!-- Authentication Links -->
             @guest
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ 'log' }}">Login</a></li>
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ URL::to('register1') }}">Register</a></li>
+                <li class="nav-item">
+                    {{-- formerly href="{{ route('login') }} --}}
+                  <a class="nav-link js-scroll-trigger" href="{{ 'log' }}">Login</a>
+                </li>
+                <li class="nav-item">
+                    {{-- formerly href="{{ route('register') }} --}}
+                  <a class="nav-link js-scroll-trigger" href="{{ URL::to('register1') }}">Register</a>
+                </li>
             @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                        {{ Auth::user()->firstName }} {{ Auth::user()->lastName }}
+                        <span class="caret"></span>
+                    </a>
 
-
+                    <ul class="dropdown-menu">
                         <li class="nav-item">
-                            <a class="nav-link js-scroll-trigger" href="{{ route('logout') }}"
+                            <a href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                          document.getElementById('logout-form').submit();">
-                                LOGOUT
+                                Logout
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 {{ csrf_field() }}
                             </form>
                         </li>
-
+                    </ul>
                 </li>
             @endguest
-        </ul>
 
+        </ul>
 
       </ul>
     </div>
