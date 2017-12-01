c021264208fd9f38c3a431535b256c321dc8ed66
Tayyab Ahmed (taa632) <t.a@usask.ca>
2017-11-30 18:20:34 -0600

    updated Appointment.php, and .gitignore files | added doctor_id.php and nsid.php files

--- wwwbs/app/Appointment.php
+++ wwwbs/app/Appointment.php
@@ -4,8 +4,33 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Appointment extends Model
+class Appointment extends Model, Authenticatable
 {
+
+	    use Notifiable;
+
+	    /**
+	     * The attributes that are mass assignable.
+	     *
+	     * @var array
+	     */
+	    protected $fillable = [
+	        'doctor_id', 'date', 'time', 'brief-desc', 'full-desc',
+	        'completed', 'cancelled', 'nsid',
+	    ];
+
+	    // This links the Appointments table with the Users table - a User can have many Appointments
+	    public function nsid()
+	    {
+	        return $this->belongsToMany(nsid::class);
+	    }
+
+		// This links the Appointments table with the Users table - a User (doctor) can have many Appointments
+	    public function doctor_id()
+	    {
+	        return $this->belongsToMany(doctor_id::class);
+	    }
+
 
 	/*
 	 / TODO: Declare $fillable and $guarded variables to prevent security attacks. Buuut,
@@ -17,10 +42,10 @@ class Appointment extends Model
 	/** TODO: DO THIS RELATIONSHIP SHIT
      / Get the doctor that is assigned to the appointment.
      / setup the relationship between Appointment and Doctor: one Appointment has one Doctor
-     / TODO: Sacalability - change this so an appointment can have multiple doctors, like for 
+     / TODO: Sacalability - change this so an appointment can have multiple doctors, like for
      / a severe medical condition in an actual hospital that needs many doctors. For the USask
      / Wellness Centre, this one-to-one relationship will suffice.
-    
+
     public function doctor()
     {
         return $this->belongsTo('App\Doctor');
