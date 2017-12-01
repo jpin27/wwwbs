0badf2a33b63552ed55f635e0e78921719cde460
Hibat ullah <hih050@mail.usask.ca>
2017-11-30 22:28:57 -0600

    Fixed drop down styling for bookings

--- wwwbs/resources/views/bookings.blade.php
+++ wwwbs/resources/views/bookings.blade.php
@@ -14,9 +14,43 @@
         		<input type="hidden" id="my_hidden_input">
             </div>
 
-            <div class="col-md-4 center-block mx-auto">
+            <div class="col-md-3 center-block mx-auto">
 
-                <div class="dropdown" style="margin:10px 0px 0px 0px;">
+              <select style="margin:30px 0px 0px 0px; width:300px; background-color:rgb(65,150,65); color:#ffff;">
+                     <option> Select Practitioner</option>
+                     <option value="1">Dr. Jay</option>
+                     <option value="2">Dr. Hiba</option>
+                     <option value="3">Dr. Tayyab</option>
+                     <option value="4">Dr. Thrwat</option>
+                     <option value="5">Dr. Fahd</option>
+                     <option value="6">Dr. Charles</option>
+                     <option value="7">Dr. Jesse</option>
+                     <option value="8">Dr. Brennan</option>
+
+            </select>
+
+            <select style="margin:30px 0px 0px 0px; width:300px; background-color:rgb(65,150,65); padding:150px; color:#ffff;">
+                   <option>Select time</option>
+                   <option value="9">9:30am</option>
+                   <option value="10">10:00am</option>
+                   <option value="11">10:30am</option>
+                   <option value="12">11:00am</option>
+                   <option value="13">11:30am</option>
+          </select>
+
+          <select style="margin:30px 0px 0px 0px; width:300px; background-color:rgb(65,150,65); padding:150px; color:#ffff;">
+                 <option> Select Service type</option>
+                 <option value="14">Medical Appointment</option>
+                 <option value="15">Mental Health Assessment</option>
+                 <option value="16">Nutritional Counseling</option>
+                 <option value="17">Sexual Health Care</option>
+                 <option value="18">Physiotherapy</option>
+                 <option value="19">Massage Therapy</option>
+                 <option value="20">Chiropractice</option>
+                 <option value="21">Travel Immunization</option>
+        </select>
+
+                <!-- <div class="dropdown" style="margin:10px 0px 0px 0px;">
                     <button class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-expanded="false" type="button" style="background-color:#4C934C;width:300px;">Select Practitioner </button>
                     <ul class="dropdown-menu" role="menu">
                             <li role="presentation"><a href="#">Dr. Jay</a></li>
@@ -48,8 +82,7 @@
                         <li role="presentation"><a href="#">Nutritional Counseling</a></li>
                         <li role="presentation"><a href="#">Sexual Health Care</a></li>
                     </ul>
-                </div>
-
+                </div> -->
             </div>
 
         </div>
